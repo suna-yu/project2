8eaf7c2d146fd7d6210c65a3eb579ff2c9779fe3
suna-yu <suna99_yu@naver.com>
2024-03-25 04:03:17 +0900

    ver1.2 sns작성

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@
 // $('.popup .close').on('click', function(){
 //     $(pop).hide(500);
 // });
+
+// vision 
+$('.vision .item > div:eq(1)').css('flex-grow', '1');
+
+$('.vision .item > div').mouseenter(function() {
+    $('.vision .item > div').css('flex-grow', '0');
+    $(this).css('flex-grow', '1');
+}).mouseleave(function() {
+    if (!$(this).is('.vision .item')) {
+        $(this).css('flex-grow', '1');
+    }
+});
